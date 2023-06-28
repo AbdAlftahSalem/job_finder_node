2020fa2c019f7957c4d249040cbfd7fd37da38a5
AbdAlftahSalem <abd321alftah@gmail.com>
2023-06-28 13:30:37 +0300

    Finish auth v1 and category and sub category

--- routs/sub_category_rout.js
+++ routs/sub_category_rout.js
@@ -5,11 +5,12 @@ const validator = require("../utils/validators/sub_category_validator")
 const {
     AddSubCategory, getAllSubCategories
 } = require("../controller/sub_category_controller")
+const Auth = require("../controller/auth_controllers");
 
 
 const router = express.Router();
 
 
-router.route("/get_sub_categories").get(getAllSubCategories)
-router.route("/add_sub_categories").post(validator.addSubCategory ,AddSubCategory)
+router.route("/get_sub_categories").get(AuthController.protectRout, getAllSubCategories)
+router.route("/add_sub_categories").post(validator.addSubCategory, ((req, res, next,) => Auth.protectRout(req, res, next, ["manager"])), AddSubCategory)
 module.exports = router;
