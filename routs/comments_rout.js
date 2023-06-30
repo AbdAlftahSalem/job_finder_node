4aea41c911ccf15e7b753786aa4e450c5f421950
AbdAlftahSalem <abd321alftah@gmail.com>
2023-06-30 20:30:57 +0300

    Add validator in comments

--- routs/comments_rout.js
+++ routs/comments_rout.js
@@ -1,5 +1,5 @@
 const express = require("express")
-// const validator = require("../utils/validators/category_validator")
+const validator = require("../utils/validators/comment_validator")
 const Auth = require("../controller/auth_controllers")
 
 const {
@@ -10,7 +10,7 @@ const {
 const router = express.Router();
 
 
-router.route("/add-comment").post(Auth.protectRout, addComment)
-router.route("/comment-remove").post(Auth.protectRout, removeComment)
-router.route("/comment-update").post(Auth.protectRout, updateComment)
+router.route("/add-comment").post(Auth.protectRout, validator.addComment, addComment)
+router.route("/comment-remove").post(Auth.protectRout, validator.removeComment, removeComment)
+router.route("/comment-update").post(Auth.protectRout, validator.removeComment, updateComment)
 module.exports = router;
